7898610c8f6fe0b1d48dcb5ced09bbcd40aa90f1
Leon™ <leontm.dev@gmail.com>
2023-08-26 18:19:59 +0200

    Added imports and an app

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 // Imports
+
+const express = require("express");
+const env = require("dotenv").config();
+const cors = require("cors");
+const path = require("path");
+
+// App
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  cors({
+    methods: ["GET", "POST", "DELETE", "PATCH"],
+    origin: "*",
+  })
+);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.listen(2000, () => {
+  console.log("Server running on port 2000");
+});

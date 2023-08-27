67663f796d633418a04b2fcf61b0459f863982d0
Leon™ <leontm.dev@gmail.com>
2023-08-27 13:22:21 +0200

    Created the users api

---
+++ routes/api/users.js
@@ -0,0 +1,15 @@
+// Imports
+
+const express = require("express");
+const Utilities = require("ltm-utilities");
+const fs = require("fs-extra");
+
+// Router
+
+const router = express.Router();
+
+// Routes
+
+// Exports
+
+module.exports = router;

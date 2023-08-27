8e6ca5055c610cd36b65e38ecc4ecff2404e26c5
Leon™ <leontm.dev@gmail.com>
2023-08-27 19:28:07 +0200

    Added API Routes

--- server.js
+++ server.js
@@ -11,17 +11,23 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
-  cors({
-    methods: ["GET", "POST", "DELETE", "PATCH"],
-    origin: "*",
-  })
+    cors({
+        methods: ["GET", "POST", "DELETE", "PATCH"],
+        origin: "*",
+    })
 );
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.listen(3000, () => {
-  console.log("Server running on port 3000");
+    console.log("Server running on port 3000");
 });
 
 // Routes
 
+// API
+
+const PACKAGES = require("./routes/api/packages");
+app.use("/api/packages", PACKAGES);
+const USERS = require("./routes/api/users");
+app.use("/api/users", USERS);
